1b990ad34776e966e9b87ced5195097b760953f8
Kazuki Fujii <68278821+okoge-kaz@users.noreply.github.com>
2024-07-16 09:22:16 +0900

    Fix [critical] AdamW epslion value

--- src/llama_recipes/arguments.py
+++ src/llama_recipes/arguments.py
@@ -382,8 +382,8 @@ def _add_regularization_args(parser: argparse.ArgumentParser) -> argparse.Argume
         help='Second coefficient for computing running averages of gradient and its square'
     )
     group.add_argument(
-        '--adam-eps', type=float, default=1e-06,
-        help='Term added to the denominator to improve numerical stability'
+        '--adam-eps', type=float, default=1e-08,
+        help='Term added to the denominator to improve numerical stability
     )
 
     return parser
